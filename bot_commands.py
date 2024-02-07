dc222cdf8e36901c0067f7f724779070253837a5
Ivan Maslennikov <roma.axe.99@gmail.com>
2024-02-07 21:29:18 +0300

    create
    create, first step

---
+++ bot_commands.py
@@ -0,0 +1,94 @@
+import telebot
+import csv
+import os
+
+# with open('token.txt', 'r') as token_file:
+#     TOKEN = token_file.read()
+
+TOKEN = '6302412593:AAEgDhdSw1_GJCFRhCFcofdyFh1-cpkJxNQ'
+bot = telebot.TeleBot(TOKEN)
+
+
+def add_item(user_id, item):
+    user_directory = f'TODO/{user_id}'
+    os.makedirs(user_directory, exist_ok=True)
+    print(user_directory)
+
+
+    # with open(f'{user_directory}/tasks.csv', 'a+', newline='', encoding='utf-8') as tasks_file:
+    with open('tasks.csv', 'a+', newline='', encoding='utf-8') as tasks_file:
+        tasks_file.seek(0)
+        data = list(csv.DictReader(tasks_file))
+
+        new_id = 1 if not data else int(data[-1]['id']) + 1
+        writer = csv.DictWriter(tasks_file, fieldnames=('id', 'discription'))
+        if not data:
+            writer.writeheader()
+        tasks_file.seek(0, 2)
+        writer.writerow({'id': new_id, 'discription': item})   
+    
+    bot.send_message(user_id, f'Задача {item}, была успешно создана! Под номером {new_id}')
+
+
+# def add_item(user_id, task_description):
+    # Путь к директории и файлу, куда записываются задачи
+    # tasks_path = 'tasks.csv'
+    
+    # # Создание директории и файла, если они еще не существуют
+    # os.makedirs(os.path.dirname(tasks_path), exist_ok=True)
+    
+    # # Открытие файла для записи новой задачи
+    # with open(tasks_path, 'a', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow([task_description])  # Запись описания задачи
+
+
+    # with open(f'TODO/{user_id}/tasks.csv', 'r+', newline='') as tasks_file:
+    #     # для номера новой задачи выбираем номер последней задачи +1
+    #     data = list(csv.DictReader(tasks_file))
+    #     if data:
+    #         new_id = int(data[-1]['id']) + 1
+    #     else:
+    #         new_id = 1
+    #     csv.DictReader(tasks_file, fieldnames=('id', 'description')).writerow({'id': new_id, 'description': item})
+    # bot.send_message(user_id, f'Задача {item} была успешно добавлены в ваш список под номеремо {new_id}')
+
+
+def show_all_items(user_id):
+    task_list = ''
+    with open('tasks.csv', 'r', newline='') as tasks_file:
+        reader = csv.DictReader(tasks_file)
+        for row in reader:
+            task_list += f"{row['id']}, {row['discription']}\n"
+    if task_list:
+        bot.send_message(user_id, task_list)
+    else:
+        bot.send_message(user_id, 'У вас ещё нет задач!')
+
+
+def delete_item(user_id, item_id):
+    new_data = []
+    found = False
+
+    with open('tasks.csv', 'r', newline='') as inp:
+        for row in csv.DictReader(inp):
+            if row['id'] == item_id:
+                found = True
+                print('Yes inp')
+            else:
+                new_data.append(row)
+                print(new_data)
+
+
+    if found:
+        with open('tasks.csv', 'w', newline='') as out:
+            writer = csv.DictWriter(out, fieldnames=('id', 'discription'))
+            writer.writeheader()
+            writer.writerows(new_data)
+        bot.send_message(user_id, f'Задача по номером {item_id} была успешно удалена!')
+    else:
+        bot.send_message(user_id, f'Задачи под номером {item_id} нет в вашем списке!')
+
+
+def refresh_tasks(user_id): # update number ls
+    pass
---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import bot_commands as commands
+
+bot = commands.bot
+
+
+COMMAND_MAP = {
+    'new_item': commands.add_item,
+    'all': commands.show_all_items,
+    'delete': commands.delete_item,
+    'refresh': commands.refresh_tasks,
+}
+
+
+NP_COMMAND = ('all', 'refresh')
+
+INFO = '''
+
+/start - идентификация пользователя
+/new_item <название задачи> - добавить новую задачу
+/all - посмотреть весь список задач
+/delete <номер задачи> - удалить задачу
+/refresh - изменить номер списка задач на номера от 1 по порядку
+/info - информация о боте
+'''
+
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    user_id = message.from_user.id
+    bot.send_message(user_id, f'Добро пожаловать, {message.from_user.first_name}')
+    task_dir = f'TODO/{user_id}'
+    if not os.path.exists(f'TODO/{user_id}'):
+        # отсылаем информацию пользователю о боте, если то ещё не был идентифицирован
+        bot.send_message(user_id, INFO)
+        os.makedirs(f'TODO/{user_id}', exist_ok=True)
+        with open (os.path.join(task_dir, 'tasks.csv'), 'w', newline='') as tasks_file:
+            # add title
+            commands.csv.writer(tasks_file).writerow(('id', 'description'))
+
+    
+@bot.message_handler(commands=['info'])
+def info_handler(message):
+    bot.send_message(message.from_user.id, INFO)
+
+
+@bot.message_handler(commands=COMMAND_MAP.keys())
+def commands_handler(message):
+    user_id = message.from_user.id
+    if os.path.exists(f'TODO/{user_id}'):
+        params = message.text.split(' ', 1)
+        command_name = params.pop(0)[1:]
+        if command_name in NP_COMMAND:
+            COMMAND_MAP[command_name](user_id)
+        else:
+            try:
+                COMMAND_MAP[command_name](user_id, *params)
+            except TypeError:
+                bot.send_message(user_id, 'Команда была использована неправильно. Воспользуйтесь командой /info')
+    else:
+        bot.send_message(user_id, 'Пользователь не инициализирован! /start')
+
+
+bot.polling()
